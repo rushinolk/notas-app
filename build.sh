786a259ca0713f5e72ba1c739ed9980702313aa0
rushinolk <arthurmachado7191@hotmail.com>
2025-04-24 12:43:40 -0300

    docker unico

--- build.sh
+++ build.sh
@@ -1,16 +1,5 @@
-
-
 ECR_REGISTRY="572048499167.dkr.ecr.us-east-1.amazonaws.com"
-REPO_NAME="aws_notas"
-REGION="us-east-1"
-aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ECR_REGISTRY
-
-
-docker build -t $REPO_NAME:backend -f notas-app/frontend/Dockerfile .
-docker tag $REPO_NAME:backend $ECR_REGISTRY/$REPO_NAME:backend
-docker push $ECR_REGISTRY/$REPO_NAME:backend 
-
-docker build -t $REPO_NAME:frontend -f notas-app/backend/Dockerfile .
-docker tag $REPO_NAME:frontend $ECR_REGISTRY/$REPO_NAME:frontend 
-docker push $ECR_REGISTRY/$REPO_NAME:frontend 
-
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
+docker build -t aws_notas .
+docker tag aws_notas:latest $ECR_REGISTRY/aws_notas:latest
+docker push $ECR_REGISTRY/aws_notas:latest
