2f12486d97c98edda6ce8f2779492de551a77915
rushinolk <arthurmachado7191@hotmail.com>
2025-05-15 12:52:19 -0300

    build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ ECR_REGISTRY="572048499167.dkr.ecr.us-east-1.amazonaws.com"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
 docker build -t aws_notasapp .
 docker tag aws_notasapp:latest $ECR_REGISTRY/aws_notasapp:latest
-docker push $ECR_REGISTRY/aws_notasapp:latest
+docker push $ECR_REGISTRY/aws_notasapp:
